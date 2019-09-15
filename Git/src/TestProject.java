fa242ab758d990502e35c98491a88eacd3a67195
samdasuza <samdasuza1234>
2019-09-15 15:35:05 -0400

    Print

--- Git/src/TestProject.java
+++ Git/src/TestProject.java
@@ -1,7 +1,9 @@
 
-public class TestProject {
+public class TestProject 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 		int i =10;
